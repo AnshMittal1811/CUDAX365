d1f5d360ac1133d0f25989b0336b63258711a766
AnshMittal1811 <anshm18111996@gmail.com>
2025-04-22 00:58:47 +0800

    Day 112 NVENC vs CPU encoding compare

--- 112_nvenc_vs_cpu/compare_encode.sh
+++ 112_nvenc_vs_cpu/compare_encode.sh
@@ -10,5 +10,4 @@ ffmpeg -y -f rawvideo -pix_fmt rgb24 -s 1920x1080 -r 30 -i frame_%04d.rgb \
   -c:v libx264 -preset veryfast out_cpu.mp4 2> cpu.log
 
 grep -E "fps=" nvenc.log | tail -n 1
-
 grep -E "fps=" cpu.log | tail -n 1
